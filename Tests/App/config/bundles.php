6bc38351bf19ef83846fc2fe97584a8afe24ec0c
adrien-mallet-iad <adrien.mallet@iadinternational.com>
2020-01-13 00:55:05 +0100

    feat(test) Add bundle to test

--- Tests/App/config/bundles.php
+++ Tests/App/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Iad\Bundle\DbDecorationBundle\DbDecorationBundle::class => ['dev' => true, 'test' => true],
 ];
